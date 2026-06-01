8ea57b72f0cdb77f84291add4d2a72ea101dcdc5
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-01 11:36:17 +0530

    implementing error class

---
+++ backend/src/utils/error.utils.js
@@ -0,0 +1,10 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        statusCode = this.statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+module.exports = AppError;
