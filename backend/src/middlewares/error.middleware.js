88588276ac55d9768491faf2ab5fecaa2495c0e9
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-01 11:45:10 +0530

    implementing error middleware to send error

---
+++ backend/src/middlewares/error.middleware.js
@@ -0,0 +1,10 @@
+const errorMiddleware = (err,req,res,next) => {
+  err.message = err.message || "Somthing went wrong"
+  err.statusCode = err.statusCode || 500
+  
+  res.status(500).json({ 
+    success:false,
+    message: err.message,
+    stack:err.stack
+    });
+}
