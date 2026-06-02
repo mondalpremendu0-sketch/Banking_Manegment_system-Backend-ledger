ff30964ab716f832a39306bc1cadac46676b56cc
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-02 12:41:57 +0530

    auth middleware all test cases passed

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,24 +1,24 @@
-const jwt = require('jsonwebtoken');
-const AppError = require('../utils/error.utils.js')
+const jwt = require("jsonwebtoken");
+const AppError = require("../utils/error.utils.js");
 
-async function isLogedIn(req,res,next) {
-  try {
-    const { token } = req.cookies;
-    if (!token) {
-      return next(new AppError("Unauthorised!!", 400));
+async function isLogedIn(req, res, next) {
+    try {
+        const { token } = req.cookies;
+        if (!token) {
+            return next(new AppError("Unauthorised!!", 400));
+        }
+
+        const verifiedUser = await jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!verifiedUser) {
+            return next(new AppError("Invalid token,Unauthorised!!", 400));
+        }
+
+        req.user = verifiedUser;
+        next();
+    } catch (err) {
+        return next(new AppError(err.message, 500));
     }
-    
-    const verifiedUser = await jwt.verify(token,process.env.JWT_SECRET)
-    
-    if (!verifiedUser) {
-      return next(new AppError("Invalid token,Unauthorised!!", 400));
-    }
-    
-    req.user = verifiedUser;
-    next();
-  } catch (err) {
-    return next(new AppError(err.message, 500));
-  }
 }
 
 module.exports = isLogedIn;
