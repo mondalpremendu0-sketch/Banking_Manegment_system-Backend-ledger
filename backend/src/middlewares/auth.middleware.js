4b4440da9ae95dafaff0e37f04f637c8d6fa4216
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-02 12:18:29 +0530

    implement auth middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const AppError = require('../utils/error.utils.js')
+
+async function isLogedIn(req,res,next) {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return next(new AppError("Unauthorised!!", 400));
+    }
+    
+    const verifiedUser = await jwt.verify(token,process.env.JWT_SECRET)
+    
+    if (!verifiedUser) {
+      return next(new AppError("Invalid token,Unauthorised!!", 400));
+    }
+    
+    req.user = verifiedUser;
+    next();
+  } catch (err) {
+    return next(new AppError(err.message, 500));
+    
+  }
+}
