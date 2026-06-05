b60a7d5b881bb2dd32ceef9757987080275c8fe2
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-06 00:21:35 +0530

    setup transction controller

---
+++ backend/src/controllers/transaction.controller.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+async function transaction_Controller() {
+  
+  
+  
+}
+
+module.exports = transaction_Controller;
--- backend/src/routes/transaction.routes.js
+++ backend/src/routes/transaction.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const isLogedIn = require('../middlewares/auth.middleware.js');
-
+const {transaction_Controller}  = require('../controllers/')
 
 
 const transactionRouter = express.Router();
 
 
-transactionRouter.post("/",isLogedIn,transactionController);
+transactionRouter.post("/",isLogedIn,transaction_Controller);
 
 module.exports = transactionRouter;
