af40c1cfbd0767d1d2de6a0f7254e931f1f84bad
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-10 23:27:47 +0530

    nodemailer setup done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const sendEmail = require('./services/email.service.js');
+
 
 const userRoutes = require('./routes/user.routes.js')
 const accountRouter = require('./routes/account.routes.js')
@@ -22,6 +24,14 @@ app.use(cookieParser());
 app.use("/api/user",userRoutes);
 app.use("/api/user",accountRouter);
 app.use("/api/user/transaction",transactionRouter);
+
+// Example usage
+sendEmail(
+  'mondalpremendu43@gmail.com',
+  'Test Email Subject',
+  'This is a test email sent with Nodemailer using OAuth2.',
+  '<p>This is a test email sent with <b>Nodemailer</b> using OAuth2.</p>'
+);
 
 app.use(errorMiddleware);
 
